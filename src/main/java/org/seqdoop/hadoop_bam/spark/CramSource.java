cb2d2fa48e2cfe3996269baec7bbd608b4a38487
Tom White <tom@cloudera.com>
2018-04-04 15:49:20 +0100

    f

--- src/main/java/org/seqdoop/hadoop_bam/spark/CramSource.java
+++ src/main/java/org/seqdoop/hadoop_bam/spark/CramSource.java
@@ -163,7 +163,7 @@ class CramSource implements Serializable {
   private List<Long> getContainerOffsetsFromFile(Configuration conf, String path, long cramFileLength)
       throws IOException {
     try (SeekableStream seekableStream = fileSystemWrapper.open(conf, path)) {
-      Iterator<Container> it = new CramContainerIterator(seekableStream);
+      Iterator<Container> it = new CramContainerHeaderIterator(seekableStream);
       List<Long> containerOffsets = new ArrayList<Long>();
       while (it.hasNext()) {
         Container container = it.next();
